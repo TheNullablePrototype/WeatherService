99379587043eea9b754a2d0df015f4dca88beab3
Exception_ <17ruslan1997@gmail.com>
2022-08-11 12:13:26 +0300

    removed, as it turned out, a "useless" test

--- src/test/java/com/prototype/weatherservice/WeatherServiceApplicationTests.java
+++ src/test/java/com/prototype/weatherservice/WeatherServiceApplicationTests.java
@@ -1,12 +1,8 @@
 package com.prototype.weatherservice;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.prototype.weatherservice.service.location.ConfigLocation;
 import com.prototype.weatherservice.service.location.Country;
 import com.prototype.weatherservice.service.location.impl.CityImpl;
 import com.prototype.weatherservice.service.location.impl.CountryImpl;
-import com.prototype.weatherservice.utils.gson.GsonAdapters;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,37 +23,6 @@ class WeatherServiceApplicationTests {
             country.register(city);
             country.register(city);
         }, "IllegalArgumentException was expected");
-    }
-
-    @Test
-    void testExceptedExceptionOnBoundariesBeyondValueLatAndLon() {
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(ConfigLocation.class, new GsonAdapters.ConfigLocationGsonAdapter())
-                .setPrettyPrinting()
-                .create();
-
-        String jsonLat = "{\n" +
-                "\t\"name\": \"Saint-Petersburg\",\n" +
-                "\t\"country\": \"Russia\",\n" +
-                "\t\"lat\": -90.939099,\n" +
-                "\t\"lon\": 30.315877\n" +
-                "}";
-
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            gson.fromJson(jsonLat, ConfigLocation.class);
-        });
-
-        String jsonLon = "{\n" +
-                "\t\"name\": \"Saint-Petersburg\",\n" +
-                "\t\"country\": \"Russia\",\n" +
-                "\t\"lat\": -83.939099,\n" +
-                "\t\"lon\": 185.315877\n" +
-                "}";
-
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            gson.fromJson(jsonLon, ConfigLocation.class);
-        });
-
     }
 
 }
